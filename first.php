bb866cdb01d0195da70166ac7bde48b04283b44b
Ayesha Mehmood <ayeshauni99@gmail.com>
2024-10-23 18:52:44 +0500

    first commit

---
+++ first.php
@@ -0,0 +1,13 @@
+<html>
+
+<head>
+    <title>My First Webpage</title>
+</head>
+<body>
+<?php
+echo "Hello php";
+echo "<h1><i>Hello Ayesha</i></h1>";
+echo 2457;
+?>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,61 @@
+<?php
+require "connection.php";
+if(isset($_POST["submit"])){
+    $name = $_POST["name"];
+    $age = $_POST["age"];
+    $country = $_POST["country"];
+    $gender = $_POST["gender"];
+    $languages = $_POST["languages"];
+    $language = "" ;
+    foreach($languages as $row){
+        $language .= $row . ",";
+    }
+    $query ="INSERT INTO user VALUES('' , '$name', '$age' , '$country', '$gender' , '$language')";
+    mysqli_query($conn, $query);
+    echo"
+    <script>
+    alert('Data Inserted');
+    </script>
+    ";
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Php data form</title>
+</head>
+<!-- <style>
+    form{
+        border: "solid 1px red"
+    }
+</style> -->
+<body>
+
+<form class="" action="" method="post" autocomplete="">
+<label for="">Name</label>
+ <input type="text"  name="name" required>
+ <br>
+ <label for="">Age</label>
+ <input type="text"  name="age" required> 
+ <br>
+ <label for="">country</label>
+ <select name="country" id="">
+    <option value="">Select a country</option>
+    <option value="USA">USA</option>
+    <option value="UK">UK</option>
+    <option value="Spain">Spain</option>
+ </select>
+ <br>
+ <label for="">Gender</label>
+ <input type="radio"   name="gender" value="Male" required>Male
+ <input type="radio"  name="gender" value="Female" required>Female
+ <br>
+ <label for="">Languages</label>
+ <input type="checkbox" name="languages[]" value="English" >English
+ <input type="checkbox" name="languages[]" value="Spanish" >Spanish
+ <input type="checkbox" name="languages[]" value="Chinees" >Chinees
+ <br>
+ <button name="submit" type="suubmit">Submit</button>
+</form> 
+</body>
+</html>
